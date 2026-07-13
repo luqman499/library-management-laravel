8ff3f9812d2787fa30d89a2966188627ea2fe4ff
luqman499 <luqmanjaffary79@gmail.com>
2026-07-13 11:06:58 +0500

    working on the authentication

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
-use App\Http\Controllers\BorrowRecordController;
-use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Book Route
-Route::get('/book', [BookController::class, 'index'])->name('book.index');
-Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
-Route::post('/book/store', [BookController::class, 'store'])->name('book.store');
-Route::get('/book/{book}', [BookController::class, 'show'])->name('book.show');
-Route::get('/book/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
-Route::put('/book/{book}/update', [BookController::class, 'update'])->name('book.update');
-Route::delete('/book/{book}/destory', [BookController::class, 'destroy'])->name('book.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Member Route
-Route::get('/member', [MemberController::class, 'index'])->name('member.index');
-Route::get('/member/create', [MemberController::class, 'create'])->name('member.create');
-Route::post('/member/store', [MemberController::class, 'store'])->name('member.store');
-Route::get('/member/{member}', [MemberController::class, 'show'])->name('member.show');
-Route::get('/member/{member}/edit', [MemberController::class, 'edit'])->name('member.edit');
-Route::put('/member/{member}/update', [MemberController::class, 'update'])->name('member.update');
-Route::delete('/member/{member}/destroy', [MemberController::class, 'destroy'])->name('member.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-//Route Borrow Controller
-Route::resource('borrow', BorrowRecordController::class);
+require __DIR__.'/auth.php';
